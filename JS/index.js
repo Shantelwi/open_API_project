733fd70b427141f6d9d6158749b406ab9d77e442
Shantel Williams <williamsshantel35@yahoo.com>
2026-02-24 21:28:40 +0000

    API fetch completed

--- JS/index.js
+++ JS/index.js
@@ -11,48 +11,12 @@ copyright.innerHTML = `Shantel Williams ${thisYear} ©️ `;
 
 footer.appendChild(copyright);
 
-//Create Fetch 
-document.addEventListener("DOMContentLoaded", () => {
 
-    const container = document.getElementById('comics-container');
-
-    const characters = ["Spider-Man", "Avengers", "Wolverine", "Black Panther", "DeadPool"];
-
-    characters.forEach(character => {
-        //create header and list for each character
-        const charHeader = document.createElement("h3");
-        charHeader.textContent = character;
-        const charList = document.createElement("ul");
-
-         container.appendChild(charHeader);
-         container.appendChild(charList);
-
-         //fetch top 5 comics for these characters
-         fetch(`https://api.allorigins.win/raw?url=https://marvel.emreparker.com/v1/search/issues?q=${encodeURIComponent(character)}&limit=5`)
-            .then(response =>{
-                if (!response.ok) throw new Error("Request Failed");
-                return response.text();
-            })
-            .then(text => {
-                const data = JSON.parse(text);
-                console.log(data);
-                const comics = data.data?.results || [];
-
-                comics.forEach(issue =>{
-                    const li = document.createElement('li');
-                    li.textContent = issue.title;
-                    charList.appendChild(li);
-                });
-            })
-            .catch(error => console.error(`Error fetching ${character}:`, error));
-    });
-
-});
-// const comicSection = document.getElementById('comicSection');
-// const comicList = comicSection.querySelector('ul');
+// const charSection = document.getElementById('charSection');
+// const charList = charSection.querySelector('ul');
 // // console.log(comicSection);
 
-// fetch('https://marvel.emreparker.com/v1/search/issues?q=spider-man&limit=5')
+// fetch('https://www.swapi.tech/api/people?page=2&limit=10')
 //     .then(response => {
 //         if (!response.ok) {
 //             throw new Error('Request failed');
@@ -65,15 +29,103 @@ document.addEventListener("DOMContentLoaded", () => {
 //         console.log(data);
 
 //         //Display comics in List
-//         const comics = data.results || [];
-//         comicList.innerHTML = '';
+//         const characters = data.results || [];
+//         // charList.innerHTML = '';
 
-//         comics.forEach(issue => {
+//         characters.forEach(issue => {
 //             const listItem = document.createElement('li');
 //             listItem.textContent = issue.title;
-//             comicList.appendChild(listItem);
+//             charList.appendChild(listItem);
 //         })
 //     })
 //     .catch(error => {
 //         console.error('An error occurred:', error); //Handling Errors
 //     });
+
+
+//Fetch for people
+fetch('https://www.swapi.tech/api/people?page=2&limit=10')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Request failed');
+        }
+        return response.json(); // Parse the response as JSON
+    })
+    .then(data => {
+        //Do something with the data
+        console.log(data);
+
+        //Display Repositories in List
+        const charSection = document.getElementById('characters');
+
+        const charList = charSection.querySelector('ul');
+
+        for (let i = 0; i < data.results.length; i++) {
+            const char = document.createElement('li');
+
+            char.textContent = data.results[i].name;
+
+            charList.appendChild(char);
+        }
+    })
+    .catch(error => {
+        console.error('An error occurred:', error); //Handling Errors
+    });
+
+    //Fetch for Starships
+
+    fetch('https://www.swapi.tech/api/starships?page=2&limit=10')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Request failed');
+        }
+        return response.json(); // Parse the response as JSON
+    })
+    .then(data => {
+        //Do something with the data
+        console.log(data);
+
+        //Display Repositories in List
+        const shipSection = document.getElementById('starships');
+
+        const shipList = shipSection.querySelector('ul');
+
+        for (let i = 0; i < data.results.length; i++) {
+            const ship = document.createElement('li');
+
+            ship.textContent = data.results[i].name;
+
+            shipList.appendChild(ship);
+        }
+    })
+    .catch(error => {
+        console.error('An error occurred:', error); //Handling Errors
+    });
+
+
+    //Fetch for movies
+fetch('https://www.swapi.tech/api/films')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Request failed');
+        }
+        return response.json(); // Parse the response as JSON
+    })
+    .then(data => {
+        //Do something with the data
+        console.log(data);
+
+        //Display Repositories in List
+        const filmList = document.querySelector('#movies ul');
+
+        for (let i = 0; i < data.result.length; i++) {
+            const film = document.createElement('li');
+
+            film.textContent = data.result[i].properties.title;
+
+            filmList.appendChild(film);
+        }
+    })
+    .catch(error => {
+        console.error('An error occurred:', error); //Handling Errors
+    });
